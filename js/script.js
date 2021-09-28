611c6583a5917d02f6751ff9230233a6f803e7ec
rahulthambi10@gmail.com <rahulthambi10@gmail.com>
2021-09-28 16:56:08 +0530

    Jquery

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('.slide-show').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        dots: true,
+        arrows: true,
+
+    });
+  });
+
